f1881cdb2faf1b305bc505162c0004aff4f479d5
Yassine LAKHDACHI <lakhdachiyassine@gmail.com>
2023-06-02 13:08:44 +0100

    model adapted to the merged dataset

--- app.py
+++ app.py
@@ -7,60 +7,63 @@ from sklearn.feature_selection import SelectKBest, f_classif
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
-df = pd.read_csv('diabetes.csv')
 
-# Handling missing values
-df['Glucose'].fillna(df['Glucose'].mean(), inplace=True)
+def load_data():
+    df = pd.read_csv('merged_dataset.csv')
+    return df
 
-# Handling outliers
-q1 = df['BMI'].quantile(0.25)
-q3 = df['BMI'].quantile(0.75)
-iqr = q3 - q1
-lower_bound = q1 - (1.5 * iqr)
-upper_bound = q3 + (1.5 * iqr)
-df = df[(df['BMI'] >= lower_bound) & (df['BMI'] <= upper_bound)]
+def train_model(df):
+    features = ['HighBP', 'HighChol', 'CholCheck', 'BMI', 'Smoker', 'Stroke', 'HeartDiseaseorAttack',
+                'PhysActivity', 'Fruits', 'Veggies', 'HvyAlcoholConsump', 'AnyHealthcare', 'NoDocbcCost',
+                'GenHlth', 'MentHlth', 'PhysHlth', 'DiffWalk', 'Sex', 'Age', 'Education', 'Income']
+    X = df[features]
+    y = df['Outcome']
+    model = RandomForestClassifier()
+    model.fit(X, y)
+    return model
 
-# Transforming data
-df['Age'] = df['Age'].apply(lambda x: 1 if x > 30 else 0)
+def main():
+    df = load_data()
+    model = train_model(df)
 
-# Feature scaling
-scaler = MinMaxScaler()
-features = ['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age']
-df[features] = scaler.fit_transform(df[features])
+    st.title("Diabetes Risk Prediction")
+    st.write("Enter the required information to predict the risk of diabetes.")
 
-# Feature selection
-X = df.drop('Outcome', axis=1)
-y = df['Outcome']
+    high_bp = st.selectbox("High Blood Pressure", [0, 1])
+    high_chol = st.selectbox("High Cholesterol", [0, 1])
+    chol_check = st.selectbox("Cholesterol Check", [0, 1])
+    bmi = st.number_input("BMI", min_value=0.0)
+    smoker = st.selectbox("Smoker", [0, 1])
+    stroke = st.selectbox("Stroke", [0, 1])
+    heart_disease = st.selectbox("Heart Disease/Attack", [0, 1])
+    physical_activity = st.selectbox("Physical Activity", [0, 1])
+    fruits = st.number_input("Fruits Intake", min_value=0)
+    veggies = st.number_input("Vegetables Intake", min_value=0)
+    heavy_alcohol = st.selectbox("Heavy Alcohol Consumption", [0, 1])
+    healthcare = st.selectbox("Any Healthcare", [0, 1])
+    no_doc_cost = st.selectbox("No Doctor Visit Due to Cost", [0, 1])
+    gen_health = st.slider("General Health (1-5)", 1, 5, 3)
+    mental_health = st.slider("Mental Health (1-30)", 0, 30, 15)
+    phys_health = st.slider("Physical Health (1-30)", 0, 30, 15)
+    diff_walk = st.slider("Difficulty Walking (1-1)", 0, 1, 0)
+    sex = st.selectbox("Sex", [0, 1])
+    age = st.number_input("Age", min_value=0)
+    education = st.number_input("Education (Years)", min_value=0)
+    income = st.number_input("Income", min_value=0)
 
-selector = SelectKBest(f_classif, k=5)
-X_new = selector.fit_transform(X, y)
-selected_features = X.columns[selector.get_support(indices=True)]
+    input_data = np.array([high_bp, high_chol, chol_check, bmi, smoker, stroke, heart_disease,
+                          physical_activity, fruits, veggies, heavy_alcohol, healthcare, no_doc_cost,
+                          gen_health, mental_health, phys_health, diff_walk, sex, age, education, income]).reshape(1, -1)
+    prediction = model.predict(input_data)[0]
 
-# Building the model
-model = RandomForestClassifier(n_estimators=100, random_state=42)
-model.fit(X_new, y)
-st.title('Diabetes Prediction App')
-
-pregnancies = st.slider('Pregnancies', 0, 17, 3)
-glucose = st.slider('Glucose', 0, 199, 117)
-blood_pressure = st.slider('Blood Pressure', 0, 122, 72)
-skin_thickness = st.slider('Skin Thickness', 0, 99, 23)
-insulin = st.slider('Insulin', 0, 846, 30)
-bmi = st.slider('BMI', 0.0, 67.1, 32.0)
-dpf = st.slider('Diabetes Pedigree Function', 0.078, 2.42, 0.3725)
-age = st.slider('Age', 21, 81, 29)
-# Making predictions
-input_data = np.array([pregnancies, glucose, blood_pressure, skin_thickness, insulin, bmi, dpf, age]).reshape(1, -1)
-input_data = scaler.transform(input_data)
-input_data = selector.transform(input_data)
-
-prediction = model.predict(input_data)
-
-# Displaying the prediction
-if prediction == 0:
-    st.success('Congratulations! Based on the information you provided, it seems like you do not have diabetes.')
-else:
-    st.error('Sorry, based on the information you provided, it seems like you may have diabetes. We recommend consulting with a healthcare professional to discuss your options.')
+    # Displaying the prediction
+    if prediction == 0:
+        st.success('Congratulations! Based on the information you provided, it seems like you do not have diabetes.')
+    else:
+        st.error('Sorry, based on the information you provided, it seems like you may have diabetes. We recommend consulting with a healthcare professional to discuss your options.')
 
 
 
+
+if __name__ == "__main__":
+    main()
